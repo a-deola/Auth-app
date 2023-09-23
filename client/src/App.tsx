b81fea2c02a24f7061171b3a6cea38e74e8b7e63
Adeola Samuel <meetprecious1403@gmail.com>
2023-09-23 23:24:16 +0100

    feat:Install dev dependencies

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Layout from "./components/Layout";
+
+function App() {
+  return (
+    <Layout>
+      <h2 className="text-3xl text-blue-500">Hello World</h2>
+    </Layout>
+  );
+}
+
+export default App;
