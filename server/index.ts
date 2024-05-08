15e24a87f51bba560540026157cd842649c23b25
Adeola Samuel <meetprecious1403@gmail.com>
2024-05-08 16:01:13 +0100

    add server routes

--- server/index.ts
+++ server/index.ts
@@ -1,47 +1,16 @@
-import express, { Request, Response } from "express";
+import express, { Express } from "express";
 import cors from "cors";
 import { PrismaClient } from "@prisma/client";
+import { PORT } from "./secrets";
+import rootRouter from "./routes";
+import { errorMiddleware } from "./middlewares/errors";
 
-const app = express();
-const prisma = new PrismaClient();
+const app: Express = express();
+export const prisma = new PrismaClient();
+
 app.use(express.json());
 app.use(cors());
+app.use("/api", rootRouter);
+app.use(errorMiddleware);
 
-async function main() {
-  // ... you will write your Prisma Client queries here
-
-  await prisma.user.create({
-    data: {
-      username: "Alice",
-      email: "alice@prisma.io",
-      password: "12345",
-    },
-  });
-  const allUsers = await prisma.user.findMany();
-  console.log(allUsers);
-}
-
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
-
-// app.post("/api/user", (req: Request, res: Response) => {
-//   const { username, email, password } = req.body;
-//   pool
-//     .query(
-//       `INSERT INTO users (username, email, password) VALUES ('${username}', '${email}', '${password}')`
-//     )
-//     .then(() => {
-//       res.json({ success: true, message: "User created" });
-//       res.redirect("/success");
-//     })
-//     .catch((err) => res.status(500).json({ success: false, error: err }));
-// });
-
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(PORT, () => console.log("Server running on port: " + PORT));
